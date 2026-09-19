272f25bd780b3588010eea8ee45d6d874639e7f3
lailamaealmario27 <leilaalmario28@gmail.com>
2026-09-19 15:03:39 +0800

    Configure Supabase connection

--- site-config.js
+++ site-config.js
@@ -1,4 +1,4 @@
 // Fill in the public Supabase project URL and publishable/anon key after setup.
 // Never put a service_role key or admin password in this file.
-export const SUPABASE_URL = '';
-export const SUPABASE_ANON_KEY = '';
+export const SUPABASE_URL = 'https://aimocdwpaunrmmbgotby.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_YVqtyDmj09sIuU0Z4Uo0wA_UmHn0Avd';
